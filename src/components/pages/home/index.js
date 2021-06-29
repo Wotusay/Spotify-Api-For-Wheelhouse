cd0d2b48a02d8635f9b1352e159a8748918461dd
wotusay <wout.salembier@hotmail.com>
2021-06-29 23:17:48 +0200

    Added Spotify Login screen and function

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import config from '../../../config';
+import Header from '../../ui/header';
 
 const Home = () => {
-  return <div></div>;
+  const { SPOTIFY_CLIENT_ID, SPOTIFY_AUTHORIZE_URL, CALLBACK_URL } = config;
+
+  const handleLogin = () => {
+    window.location = `${SPOTIFY_AUTHORIZE_URL}?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${CALLBACK_URL}&response_type=token&show_dialog=true`;
+  };
+
+  return (
+    <div className="login">
+      <Header />
+      <input
+        onClick={handleLogin}
+        value="Login to Spotify"
+        type="submit"></input>
+    </div>
+  );
 };
 
 export default Home;
