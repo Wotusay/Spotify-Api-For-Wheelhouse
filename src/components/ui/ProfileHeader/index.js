5c9c30134d7066fa02174667338f4fdb5a076bbf
Daren Malfait <github@daren.be>
2021-09-15 11:57:25 +0200

    feat: add icon fallback for profile header image

--- src/components/ui/ProfileHeader/index.js
+++ src/components/ui/ProfileHeader/index.js
@@ -1,30 +1,43 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
 const ProfileHeader = ({ profile }) => {
-    
   const handleLogout = (e) => {
     localStorage.clear();
     window.location.reload();
-  }
+  };
 
   return (
     <div className="profile_header">
-      <img
-        className="rounded-full"
-        width="35"
-        height="35"
-        src={profile.images[0].url}
-        alt="profile"
-      />
-      <a rel="noopener noreferrer" target="_blank" href={profile.external_urls.spotify}  className="font-bold text-2xl">{profile.display_name} </a>
+      {profile.images.length > 0 ? (
+        <img
+          className="rounded-full"
+          width="35"
+          height="35"
+          src={profile.images[0].url}
+          alt="profile"
+        />
+      ) : (
+        <svg viewBox="0 0 1024 1024" aria-label="Profile">
+          <path fill="#fff" d="M730.06 679.64q-45.377 53.444-101.84 83.443t-120 29.999q-64.032 0-120.75-30.503t-102.6-84.451q-40.335 13.109-77.645 29.747t-53.948 26.722l-17.142 10.084Q106.388 763.84 84.96 802.41t-21.428 73.107 25.461 59.242 60.754 24.705h716.95q35.293 0 60.754-24.705t25.461-59.242-21.428-72.603-51.679-57.225q-6.554-4.033-18.907-10.84t-51.427-24.453-79.409-30.755zm-221.84 25.72q-34.285 0-67.561-14.873t-60.754-40.335-51.175-60.502-40.083-75.124-25.461-84.451-9.075-87.728q0-64.032 19.915-116.22t54.452-85.964 80.67-51.931 99.072-18.151 99.072 18.151 80.67 51.931 54.452 85.964 19.915 116.22q0 65.04-20.167 130.58t-53.948 116.72-81.426 83.443-98.568 32.268z"></path>
+        </svg>
+      )}
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href={profile.external_urls.spotify}
+        className="text-2xl font-bold"
+      >
+        {profile.display_name}{" "}
+      </a>
       <button onClick={(e) => handleLogout(e)}>
         <svg
           width="18"
           height="18"
           viewBox="0 0 24 24"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg">
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
             d="M9 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H9"
             stroke="white"
